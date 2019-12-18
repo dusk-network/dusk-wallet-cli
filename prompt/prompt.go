f1433695b849b3a238083dd92f0a9ed2b0d697cc
Jules de Smit <julesdesmit@gmail.com>
2019-12-18 17:09:56 +0100

    rename 'menu' package to 'prompt'; make dummy menus for the two different wallet states (loaded and not loaded)

---
+++ prompt/prompt.go
@@ -0,0 +1,69 @@
+package prompt
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/manifoldco/promptui"
+)
+
+// LoadMenu opens the prompt for loading a wallet. Returns an error
+// in case of a connectivity problem with the node.
+func LoadMenu() error {
+	// Looping until user chooses "Exit", successfully loads a wallet,
+	// or a connectivity error occurs.
+	for {
+		prompt := promptui.Select{
+			Label: "Select action",
+			Items: []string{"Create Wallet", "Load Wallet", "Load Wallet From Seed", "Exit"},
+		}
+
+		_, result, err := prompt.Run()
+		if err != nil {
+			// Prompts should always succeed.
+			panic(err)
+		}
+
+		switch result {
+		case "Create Wallet":
+			fmt.Println("Hello, world!")
+		case "Load Wallet":
+			fmt.Println("Hello, world!")
+		case "Load Wallet From Seed":
+			fmt.Println("Hello, world!")
+		case "Exit":
+			// Simply exiting here saves us from having to write a
+			// lot of complicated handling code in the main function.
+			os.Exit(0)
+		}
+	}
+}
+
+func WalletMenu() {
+	// Looping until user chooses "Exit", or a connectivity error
+	// occurs.
+	prompt := promptui.Select{
+		Label: "Select action",
+		Items: []string{"Transfer DUSK", "Stake DUSK", "Bid DUSK", "Show Balance", "Show Address", "Exit"},
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	switch result {
+	case "Transfer DUSK":
+		fmt.Println("Hello, world!")
+	case "Stake DUSK":
+		fmt.Println("Hello, world!")
+	case "Bid DUSK":
+		fmt.Println("Hello, world!")
+	case "Show Balance":
+		fmt.Println("Hello, world!")
+	case "Show Address":
+		fmt.Println("Hello, world!")
+	case "Exit":
+		os.Exit(0)
+	}
+}
